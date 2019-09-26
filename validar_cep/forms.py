9b59446f151ddb915b2338839e12566b0d896474
03654916163 <thales.t@gmail.com>
2019-09-26 11:59:24 -0300

    melhorando a forma que é exibida a resposta de validação

--- validar_cep/forms.py
+++ validar_cep/forms.py
@@ -1,34 +1,49 @@
 from django import forms
 import re
 
+from django.utils.safestring import mark_safe
+
+
 class CepForm(forms.Form):
     """form que recebe o cep digitado pelo usuário"""
 
     cep = forms.CharField(label='Cep:', max_length=6,
-                          widget=forms.TextInput(attrs={'class': 'form-control'},),
+                          widget=forms.TextInput(attrs={'class': 'form-control'}, ),
                           help_text='Digite um número de CEP válido.<br> '
                                     '1. O CEP é um número maior que 100.000 e menor que 999999<br>'
                                     '2. O CEP não pode conter nenhum nenhum dígito repetitivo alternado em pares<br>'
                                     '3. Digite somente números'
                           )
+
     def clean_cep(self):
         data = self.cleaned_data['cep']
 
         # prog = re.compile(pattern)
         # result = prog.match(string)
-        #re.sub("\D", "", "aas30dsa20")
-        #verificar se tem letras
+        # re.sub("\D", "", "aas30dsa20")
+        # verificar se tem letras
         if re.search("\D", data) is not None:
             raise forms.ValidationError("Somente números!")
 
-        #verificar se esta dentro da faixa de valores permitidos, maior que 100.000 e menor que 999999
+        # verificar se esta dentro da faixa de valores permitidos, maior que 100.000 e menor que 999999
         # data_num = float(data)
         if len(data) != 6 or data == '100000' or data == '999999':
             raise forms.ValidationError("O CEP é um número maior que 100.000 e menor que 999999!")
 
-        #'2. O CEP não pode conter nenhum nenhum dígito repetitivo alternado em pares'
+        # '2. O CEP não pode conter nenhum nenhum dígito repetitivo alternado em pares'
         if re.search("(\d)(?=\d\\1{1,1})", data) is not None:
-            raise forms.ValidationError("O CEP não pode conter nenhum nenhum dígito repetitivo alternado em pares!")
+            mens_erro = 'O CEP não pode conter nenhum nenhum dígito repetitivo alternado em pares!'
+            for match in re.finditer("(\d)(?=\d\\1{1,1})", data):
+                mens_erro += '<br> %s<span style="color: blue" >%s</span>%s<span style="color: blue" >%s</span>%s' % (
+                match.string[:match.start()],  match.string[match.start()],
+                match.string[match.start()+1], match.string[match.start()+2],
+                match.string[match.start() + 3:] if len(match.string) > match.start() + 3 else '')
+
+
+                # cep = '%s<span style="color: %s" >%s</span>%s' % (
+                # match.string[:match.start()+2], cor, match.string[match.start()+2], match.string[match.start() + 3:])
+
+            raise forms.ValidationError(mark_safe(mens_erro))
 
         # Always return a value to use as the new cleaned data, even if
         # this method didn't change it.
--- validar_cep/views.py
+++ validar_cep/views.py
@@ -10,3 +10,23 @@ class ValidarCepView(FormView):
     form_class = CepForm
     template_name = 'validar_cep/index.html'
     success_url = '/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        # context = super().get_context_data(**kwargs)
+        # context['publisher'] = self.object
+        # return super().form_valid(form)
+        # form.cleaned_data['resultado'] = "Válido"
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+    # def post(self, request, *args, **kwargs):
+    #     form = self.form_class(request.POST)
+    #     error = False
+    #     result = None
+    #     if form.is_valid():
+    #
+    #         return render(request, self.template_name, {'form': form, 'result': result, 'error': error})
+    #
+    #     return render(request, self.template_name, {'form': form})
