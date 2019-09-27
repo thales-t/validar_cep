ffb8a9cf9cf29a1b58236a35eea89f7974fc8423
03654916163 <thales.t@gmail.com>
2019-09-27 09:35:56 -0300

    correções, melhorias testes

--- validar_cep/urls.py
+++ validar_cep/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from validar_cep.views import ValidarCepView
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     path('', ValidarCepView.as_view()),
 
 ]
--- validar_cep/views.py
+++ validar_cep/views.py
@@ -15,21 +15,5 @@ class ValidarCepView(FormView):
     success_url = '/'
 
     def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        # context = super().get_context_data(**kwargs)
-        # context['publisher'] = self.object
-        # return super().form_valid(form)
-        # form.cleaned_data['resultado'] = "Válido"
+        #colocando o form no contexto
         return self.render_to_response(self.get_context_data(form=form))
-
-
-    # def post(self, request, *args, **kwargs):
-    #     form = self.form_class(request.POST)
-    #     error = False
-    #     result = None
-    #     if form.is_valid():
-    #
-    #         return render(request, self.template_name, {'form': form, 'result': result, 'error': error})
-    #
-    #     return render(request, self.template_name, {'form': form})
